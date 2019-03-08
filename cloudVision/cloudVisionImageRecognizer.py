0748f0ecbf00332dd1b97c98faba5ee119246075
grahamrichard <rgraham2@macalester.edu>
2019-03-08 01:38:18 -0600

    Created cloud vision image categorization script

---
+++ cloudVision/cloudVisionImageRecognizer.py
@@ -0,0 +1,17 @@
+from google.cloud import vision
+from google.cloud.vision import types
+
+client = vision.ImageAnnotatorClient.from_service_account_file('/Users/richardgraham/Sources/GenerativeMemes/cloudVision/Meme Generator-802734cd5cac.json')
+
+# img = '/Volumes/GoogleDrive/My Drive/3. COMP 225/MEME VAULT/fish joint.jpg'
+img = input('enter filepath to image: ')
+
+with open(img, 'rb') as inputImage:
+    content = inputImage.read()
+
+visionImage = vision.types.Image(content=content)
+recogResponse = client.web_detection(image=visionImage)
+print(recogResponse)
+
+recogContent = recogResponse.web_detection
+print(recogContent.best_guess_labels)
