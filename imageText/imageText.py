2d452b4bd57c3e9d3989786ba564b55212b51071
grahamrichard <rgraham2@macalester.edu>
2019-03-13 13:01:16 -0500

    Begin imageText classes

---
+++ imageText/imageText.py
@@ -0,0 +1,18 @@
+from PIL import Image, ImageDraw, ImageFont
+
+class Captioner:
+    def __init__(self):
+        self.font = ImageFont.truetype('./Impact.ttf', 18)
+        self.fontFill = (255, 255, 255)
+
+    def open_image(self, path):
+        return Image.open(path)
+
+    def write(self, pic, text, xPosition, yPosition):
+        writer = ImageDraw.Draw(pic)
+        ##FIXME what the fuck
+       ##  writer.text = ((10, 10), 'Hello World', font=self.font, fill=(255,255,255))
+        return pic
+
+    def save_image(self, pic, fname):
+        pic.save(fname)
