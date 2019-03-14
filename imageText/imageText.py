d991568890102b1ad9ae0cdf89ef76c393054cac
grahamrichard <rgraham2@macalester.edu>
2019-03-13 23:09:04 -0500

    toned down a facetious FIXME message...

--- imageText/imageText.py
+++ imageText/imageText.py
@@ -10,7 +10,7 @@ class Captioner:
 
     def write(self, pic, text, xPosition, yPosition):
         writer = ImageDraw.Draw(pic)
-        ##FIXME what the fuck
+        ##FIXME this should accept font & fill args as below, but it keeps throwing an error for some reason...
        ##  writer.text = ((10, 10), 'Hello World', font=self.font, fill=(255,255,255))
         return pic
 
