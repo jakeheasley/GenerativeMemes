b110e0c2773f09111589abb088058510cc392b4b
jakeheasley <jheasley@macalester.edu>
2019-03-25 13:09:49 -0500

    Created Markov chain class

---
+++ Markov_Object/Chain_Debug.py
@@ -0,0 +1,15 @@
+from Markov_Object.Markov_Chain import Chain
+
+
+filepath = "testing.txt"
+chars = 140
+tries = 200
+ratio = .5
+
+test_chain = Chain(chars=chars, tries=tries, ratio=ratio, filepath=filepath)
+print(test_chain.get_chars())
+print(test_chain.get_ratio())
+print(test_chain.get_tries())
+print(test_chain.get_filepath())
+
+print(test_chain.make_sent(1))
