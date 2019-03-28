5173693fca3ac14a5deba7c250126b3d988b0ba1
grahamrichard <rgraham2@macalester.edu>
2019-03-27 20:41:00 -0500

    Documented Bot_Initializer.py

--- Twitter_Bot/Bot_Initializer.py
+++ Twitter_Bot/Bot_Initializer.py
@@ -1,9 +1,10 @@
 from Twitter_Bot.Bot import Bot
 import json
 
-# Minneapolis ID
+# Minneapolis ID (location for trends near us)
 location = 23424977
 
+# authenticate the bot w/ login credentials from external json
 with open('twitter_credentials.json') as cred_data:
     info = json.load(cred_data)
     consumer_key = info['CONSUMER_KEY']
@@ -11,8 +12,11 @@ with open('twitter_credentials.json') as cred_data:
     access_key = info['ACCESS_TOKEN']
     access_secret = info['ACCESS_SECRET']
 
+# instantiate bot
 bot1 = Bot(consumer_key, consumer_secret, access_key, access_secret)
 bot1.rate_status()
+
+# trends testing
 trends = bot1.get_trends(location)[0]['trends']
 test = []
 for trend in trends:
