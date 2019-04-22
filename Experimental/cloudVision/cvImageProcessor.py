dd5ba7050dcf21b09e5af66ead6d6da5b529c624
grahamrichard <rgraham2@macalester.edu>
2019-04-22 14:39:00 -0500

    Added method for handling binary input

--- Experimental/cloudVision/cvImageProcessor.py
+++ Experimental/cloudVision/cvImageProcessor.py
@@ -8,11 +8,19 @@ class CV:
         self.client = vision.ImageAnnotatorClient.from_service_account_file("./GCV_login.json")
 
     def open_image(self, path):
-        """open a given file and return its data for processing"""
+        """open a given file and return its data for processing
+        @:param path: filepath to open
+        @:return vision_image: vision.types.Image - this seems to be different than normal image binary"""
         with open(path, 'rb') as imageFile:
             image_data = imageFile.read()
         vision_image = vision.types.Image(content=image_data)
         return vision_image
+
+    def open_blob(self, blob):
+        """convert binary image data for GCV.
+        @:param blob: binary image data, i.e. from python's standard open() method. TODO: or from SQL?
+        @:return vision.types.Image - this seems to be different than normal image binary"""
+        return vision.types.Image(content=blob)
 
     def run_ocr(self, imgdata, mode='text'):
         """takes in image data, sends it to Google CV for OCR, returns text in the image
