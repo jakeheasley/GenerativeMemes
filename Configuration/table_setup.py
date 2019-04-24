9c6a018fe18f51636f0a6dc162d1f3347b7e134b
Joseph Wriedt <jwriedt@macalester.edu>
2019-04-24 15:56:50 -0500

    Began testing/setting up Cloud Vision.

--- Configuration/table_setup.py
+++ Configuration/table_setup.py
@@ -8,3 +8,7 @@ cursor.execute("create database Memes;")
 cursor.execute("use Memes;")
 cursor.execute("create table Tweets (Author text, Tweet longtext, id bigint, trend text, date_pulled date, tweet_date date, Primary key (id));")
 cursor.close()
+
+"""use Memes;
+create table ocr (photo longblob, label longtext, id int auto_increment, primary key(id));
+create table ocr_similar (photo_text varchar(140), photo_id int, foreign key(photo_id) references ocr(id), primary key(photo_text, photo_id));"""
--- Prototype/SQL.py
+++ Prototype/SQL.py
@@ -1,6 +1,8 @@
 import mysql.connector as mysql
 
+#SQl class for interacting with the MYSQL database
 class SQL:
+    #intializing and connecting to database
     def __init__(self, host, port, username, password, database):
         self.host = host
         self.port = port
@@ -19,11 +21,42 @@ class SQL:
 
         self.cursor = self.db.cursor()
 
-    def insertion(self, tweet_tuple):
+    #inserting tweets tuples into MYSQL
+    def insertion_tweets(self, tweet_tuple_list):
         sql_insert = """insert ignore into Tweets (Author, Tweet, id, trend, date_pulled, tweet_date) values (%s,%s,%s,%s,%s,%s);"""
         self.cursor.executemany(sql_insert, tweet_tuple)
         self.db.commit()
 
+    #inserts the photo with blob and returns the id of the insertion
+    #format of photo_tuple is (photo blob, photo label, photo_text)
+    def insertion_photo(self, temp_blob, temp_label, temp_text):
+        temp_id = self.photo_id_search(temp_label)
+        self.cursor.execute("insert ignore into ocr (photo, label, id) values (%s,%s,%s);",(temp_blob,temp_label, temp_id))
+        self.db.commit()
+        return(temp_id)
+
+    def insertion_similar_photo(self, photo_text_list, id):
+        tuple_list = []
+        for x in photo_text_list:
+            tuple_list.append((id,x))
+        sql_insert = "insert ignore into ocr_similar (photo_text, photo_id) values (%s,%s);"
+        self.cursor.executemany(sql_insert, tuple_list)
+        self.db.commit()
+
+
+    #helper method with finding correct id for label
+    def photo_id_search(self, label):
+        sql_query = "select id from ocr where label = %s"
+        self.cursor.execute(sql_query,(label,))
+        temp = self.cursor.fetchone()
+        if (temp == None):
+            self.cursor.execute("select count(id) from ocr;")
+            temp = self.cursor.fetchone()
+            if (temp == None):
+                return(0)
+        return(temp[0])
+
+    #query tweets from database based on author's handle
     def author_query(self, author):
         query = """select Tweet from Tweets where author =  %s"""
         self.cursor.execute(query, (author,))
@@ -32,7 +65,8 @@ class SQL:
             query_list.append(x[0])
         return query_list
 
-    def trend_query(self, trend):
+    #query tweets where the tag is passed as parameter
+    def tag_query(self, trend):
         query = """select Tweet from Tweets where tag =  %s;"""
         self.cursor.execute(query, (trend,))
         query_list = []
@@ -40,5 +74,6 @@ class SQL:
             query_list.append(x[0])
         return query_list
 
+    #closes connection with database do so when done with object
     def close(self):
         self.cursor.close()
