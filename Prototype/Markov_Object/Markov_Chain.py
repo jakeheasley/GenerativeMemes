abd6bbf9960a39dc9db3f2bce8270efc01ef03ba
jakeheasley <jheasley@macalester.edu>
2019-04-30 09:05:35 -0500

    Deleted Spacy import

--- Prototype/Markov_Object/Markov_Chain.py
+++ Prototype/Markov_Object/Markov_Chain.py
@@ -1,17 +1,5 @@
 import markovify
 import re
-import spacy
-
-nlp = spacy.load("en")
-
-
-class POSifiedText(markovify.Text):
-    def word_split(self, sentence):
-        return ["::".join((word.orth_, word.pos_)) for word in nlp(sentence)]
-
-    def word_join(self, words):
-        sentence = " ".join(word.split("::")[0] for word in words)
-        return sentence
 
 
 class Chain:
