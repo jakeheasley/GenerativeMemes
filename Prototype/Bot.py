eea3073cae89a65a10961d8ba1908506c5b5417e
jakeheasley <jheasley@macalester.edu>
2019-05-07 14:16:56 -0500

    changed logic to work

--- Prototype/Bot.py
+++ Prototype/Bot.py
@@ -78,7 +78,7 @@ class Bot:
         return tweets
 
     def get_status(self, tweet_id):
-        tweet = self.api.statuses_lookup([tweet_id])
+        tweet = self.api.statuses_lookup([tweet_id])[0]
         tweet_text = re.sub("https:.*$", "", tweet.full_text)
         tweet_text = re.sub("&amp", "&", tweet_text)
 
